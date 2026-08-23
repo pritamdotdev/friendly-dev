341811226eb9977aded0ca165ce470758ea977ce
Pritam Paul <pritampaul5530@gmail.com>
2026-08-23 23:42:27 +0530

    Added loaders to fetch projects

--- app/routes.ts
+++ app/routes.ts
@@ -7,7 +7,7 @@ import {
 
 export default [
   layout("./routes/layouts/home.tsx", [index("routes/home/index.tsx")]),
-  layout("./routes/layout/main.tsx", [
+  layout("./routes/layouts/main.tsx", [
     route("about", "./routes/about/index.tsx"),
     route("contact", "./routes/contact/index.tsx"),
     route("projects", "./routes/projects/index.tsx"),
--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,4 +1,17 @@
-const ProjectsPage = () => {
+import type { Route } from "./+types/index";
+import type { Project } from "~/types";
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  const res = await fetch("http://localhost:8000/projects");
+  const data = await res.json();
+
+  return { projects: data };
+}
+
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData as {projects: Project[]};
   return (
     <>
       <h2 className="text-3xl font-bold mb-8">🚀 Projects</h2>
