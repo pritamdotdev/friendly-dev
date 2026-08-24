c7f9dea3b7459cead73b23bcfb6447aa1af8bb62
Pritam Paul <pritampaul5530@gmail.com>
2026-08-24 23:09:48 +0530

    Updated Projects page

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,3 +1,4 @@
+import ProjectCard from "~/components/ProjectCard";
 import type { Route } from "./+types/index";
 import type { Project } from "~/types";
 
@@ -11,10 +12,16 @@ export async function loader({
 }
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
-  const { projects } = loaderData as {projects: Project[]};
+  const { projects } = loaderData as { projects: Project[] };
   return (
     <>
       <h2 className="text-3xl font-bold mb-8">🚀 Projects</h2>
+
+      <div className="grid gap-6 sm:grid-cols-2">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </>
   );
 };
