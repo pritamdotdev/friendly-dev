f80996db55c1c5b7316c8a3d8160d1bd5f5a1d19
Pritam Paul <pritampaul5530@gmail.com>
2026-08-29 23:53:09 +0530

    Fixed/Added new component for better readability

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router";
 
-const Hero = ({ name = "[NAME]" }) => {
+type HeroProps = {
+  name?: string;
+  text?: string;
+};
+
+const Hero: React.FC<HeroProps> = ({ name = "[NAME]" }) => {
   return (
     <header className="text-center py-20 px-4 bg-gray-900 text-white transition-colors duration-300">
       <h2 className="text-4xl font-bold mb-4">Hey! I'm {name}👋</h2>
--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ProjectCard from "~/components/ProjectCard";
+import Pagination from "~/components/Pagination";
 import type { Route } from "./+types/index";
 import type { Project } from "~/types";
 
@@ -26,20 +27,6 @@ const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
   const indexOfFirst = indexOfLast - projectsPerPage;
   const currentProjects = projects.slice(indexOfFirst, indexOfLast);
 
-  //Pagination button render
-  const renderPagination = () => (
-    <div className="flex justify-center gap-2 mt-2">
-      {Array.from({ length: totalPages }, (_, idx) => (
-        <button
-          key={idx + 1}
-          className={`px-3 py-1 cursor-pointer rounded ${currentPage === idx + 1 ? "bg-blue-600 text-white" : "bg-gray-700 text-gray-200"}`}
-          onClick={() => setCurrentPage(idx + 1)}
-        >
-          {idx + 1}
-        </button>
-      ))}
-    </div>
-  );
   return (
     <>
       <h2 className="text-3xl font-bold mb-8">🚀 Projects</h2>
@@ -49,7 +36,11 @@ const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
           <ProjectCard key={project.id} project={project} />
         ))}
       </div>
-      {totalPages > 1 && renderPagination()}
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </>
   );
 };
