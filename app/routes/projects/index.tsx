b3b4b4d4f78a4862c7acb25ccbd4a86a2cb915f5
Pritam Paul <pritampaul5530@gmail.com>
2026-08-31 23:45:22 +0530

    Buttons added for a simplistic view

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -14,22 +14,51 @@ export async function loader({
 }
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
-  const { projects } = loaderData as { projects: Project[] };
-
+  //
+  const [selectedCategory, setSelectedCategory] = useState("All");
   const [currentPage, setCurrentPage] = useState(1);
   const projectsPerPage = 4;
 
+  const { projects } = loaderData as { projects: Project[] };
+
+  // Get unique categories
+  const categories = [
+    "All",
+    ...new Set(projects.map((project) => project.category)),
+  ];
+
+  // Filter project based on the category
+  const filteredProjects =
+    selectedCategory === "All"
+      ? projects
+      : projects.filter((project) => project.category === selectedCategory);
+
   //Calculate total pages
-  const totalPages = Math.ceil(projects.length / projectsPerPage);
+  const totalPages = Math.ceil(filteredProjects.length / projectsPerPage);
 
   //Get Current pages projects
   const indexOfLast = currentPage * projectsPerPage;
   const indexOfFirst = indexOfLast - projectsPerPage;
-  const currentProjects = projects.slice(indexOfFirst, indexOfLast);
+  const currentProjects = filteredProjects.slice(indexOfFirst, indexOfLast);
 
   return (
     <>
       <h2 className="text-3xl font-bold mb-8">🚀 Projects</h2>
+
+      <div className="flex flex-wrap gap-2 mb-8">
+        {categories.map((category) => (
+          <button
+            key={category}
+            className={`px-3 py-1 rounded text-sm cursor-pointer ${selectedCategory === category ? "bg-blue-600 text-white" : "bg-gray-700 text-gray-200"}`}
+            onClick={() => {
+              setSelectedCategory(category);
+              setCurrentPage(1);
+            }}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
 
       <div className="grid gap-6 sm:grid-cols-2">
         {currentProjects.map((project) => (
