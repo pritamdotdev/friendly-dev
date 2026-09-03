71df2482ace385081ca4dcbf8ab6a1e5d8f3587f
Pritam Paul <pritampaul5530@gmail.com>
2026-09-03 20:42:36 +0530

    Updated about and contact pages

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,8 +1,69 @@
 const ContactPage = () => {
   return (
-    <>
-      <h2 className="text-3xl font-bold mb-8 text-center">Contact Me</h2>
-    </>
+    <div className="max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900">
+      <h2 className="text-3xl font-bold mb-8 text-center text-white">📡Contact Me</h2>
+
+      <form className="space-y-6">
+        <div>
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Full Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="w-full mt-1px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="w-full mt-1px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="subject"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Subject
+          </label>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            className="w-full mt-1px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="messsage"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            className="w-full mt-1px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <button className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 cursor-pointer">
+          Send Message
+        </button>
+      </form>
+    </div>
   );
 };
 
--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,6 +1,7 @@
 import FeaturedProjects from "~/components/FeaturedProjects";
 import type { Route } from "./+types/index";
 import type { Project } from "~/types";
+import AboutPreview from "~/components/AboutPreview";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -24,6 +25,7 @@ const HomePage = ({ loaderData }: Route.ComponentProps) => {
   return (
     <>
       <FeaturedProjects projects={projects} count={2} />
+      <AboutPreview />
     </>
   );
 };
