002e7ae26d92b36281ca8b550942193bde4f7081
Pritam Paul <pritampaul5530@gmail.com>
2026-09-07 23:52:55 +0530

    Resources added for blog files

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,4 +1,19 @@
-const BlogPage = () => {
+import type { Route } from "./+types/index";
+import { Link } from "react-router";
+
+export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+  const url = new URL("/posts-meta.json", request.url);
+  const res = await fetch(url.href);
+
+  if (!res.ok) throw new Error("Failed to fetch data");
+
+  const data = await res.json();
+
+  return { posts: data };
+}
+
+const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const { posts } = loaderData;
   return (
     <>
       <h2 className="text-3xl font-bold mb-8">📝 Blog</h2>
