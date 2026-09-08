a871346e32011c14be56dd18cb27a01dcef1cc4c
Pritam Paul <pritampaul5530@gmail.com>
2026-09-08 23:16:48 +0530

    Blog page updated

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,7 +1,10 @@
 import type { Route } from "./+types/index";
-import { Link } from "react-router";
+import type { PostMeta } from "~/types";
+import PostCard from "~/components/PostCard";
 
-export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
   const url = new URL("/posts-meta.json", request.url);
   const res = await fetch(url.href);
 
@@ -15,9 +18,12 @@ export async function loader({ request }: Route.LoaderArgs): Promise<any> {
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
   const { posts } = loaderData;
   return (
-    <>
-      <h2 className="text-3xl font-bold mb-8">📝 Blog</h2>
-    </>
+    <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
+      <h2 className="text-3xl font-bold mb-8 text-white">📝 Blog</h2>
+      {posts.map((post) => (
+        <PostCard key={post.slug} post={post} />
+      ))}
+    </div>
   );
 };
 
