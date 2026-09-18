b71f63861ce3a3242452d986066e7bbcae3a5f4c
Pritam Paul <pritampaul5530@gmail.com>
2026-09-19 00:38:46 +0530

    Uploaded data to Strapi

--- app/components/FeaturedProjects.tsx
+++ app/components/FeaturedProjects.tsx
@@ -7,15 +7,14 @@ type FeaturedProjectsProps = {
 };
 
 const FeaturedProjects = ({ projects, count = 4 }: FeaturedProjectsProps) => {
-  const featured = projects
-    .filter((project) => project.featured)
-    .slice(0, count);
+  if (projects.length === 0) return null;
+
   return (
     <section>
       <h2 className="text-2xl font-bold mb-6">🌟 Featured Projects</h2>
 
       <div className="grid gap-6 sm:grid-cols-2">
-        {featured.map((project) => (
+        {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
       </div>
--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,6 +1,6 @@
 import FeaturedProjects from "~/components/FeaturedProjects";
 import type { Route } from "./+types/index";
-import type { Project } from "~/types";
+import type { Project, StrapiProject, StrapiResponse } from "~/types";
 import AboutPreview from "~/components/AboutPreview";
 import LatestPosts from "~/components/LatestPosts";
 import type { PostMeta } from "~/types";
@@ -18,7 +18,9 @@ export async function loader({
   const url = new URL(request.url);
 
   const [projectRes, postRes] = await Promise.all([
-    fetch(`${import.meta.env.VITE_API_URL}/projects`),
+    fetch(
+      `${import.meta.env.VITE_API_URL}/projects?filters[featured] [$eq]=true&populate=*`,
+    ),
     fetch(new URL("/posts-meta.json", url)),
   ]);
 
@@ -26,12 +28,24 @@ export async function loader({
     throw new Error("Failed to fetch projects or posts");
   }
 
-  const [projects, posts] = await Promise.all([
-    projectRes.json(),
-    postRes.json(),
-  ]);
+  const projectJson: StrapiResponse<StrapiProject> = await projectRes.json();
+  const postJson = await postRes.json();
 
-  return { projects, posts };
+  const projects = projectJson.data.map((item) => ({
+    id: item.id,
+    documentId: item.documentId,
+    title: item.title,
+    description: item.description,
+    image: item.image?.url
+      ? `${import.meta.env.VITE_STRAPI_URL}${item.image.url}`
+      : "/images/no-image.png",
+    url: item.url,
+    date: item.date,
+    category: item.category,
+    featured: item.featured,
+  }));
+
+  return { projects, posts: postJson };
 }
 
 const HomePage = ({ loaderData }: Route.ComponentProps) => {
