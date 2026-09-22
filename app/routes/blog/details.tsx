5504ab904ec0c7eba346e2633b6781340bad8fcd
Pritam Paul <pritampaul5530@gmail.com>
2026-09-22 23:47:05 +0530

    Created image backup in cloudinary

--- app/routes/blog/details.tsx
+++ app/routes/blog/details.tsx
@@ -1,43 +1,51 @@
 import ReactMarkdown from "react-markdown";
 import type { Route } from "./+types/details";
-import type { PostMeta } from "~/types";
+import type { PostMeta, StrapiPost, StrapiResponse } from "~/types";
 import { Link } from "react-router";
 
 export async function loader({ request, params }: Route.LoaderArgs) {
   const { slug } = params;
 
-  const url = new URL("/posts-meta.json", request.url);
-  const res = await fetch(url.href);
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/posts?filters[slug] [$eq]=${slug}&populate=image`,
+  );
 
   if (!res.ok) throw new Error("Failed to fetch data");
 
-  const index = await res.json();
+  const json: StrapiResponse<StrapiPost> = await res.json();
 
-  const postMeta = index.find((post: PostMeta) => post.slug === slug);
+  if (!json.data.length) throw new Response("Not Found", { status: 404 });
 
-  if (!postMeta) throw new Response("Not Found", { status: 404 });
+  const item = json.data[0];
 
-  //Dynamically import raw markdown
-  const markdown = await import(`../../posts/${slug}.md?raw`);
-
-  return {
-    postMeta,
-    markdown: markdown.default,
+  const post = {
+    id: item.id,
+    slug: item.slug,
+    excerpt: item.excerpt,
+    title: item.title,
+    date: item.date,
+    body: item.body,
+    image: item.image?.url ? `${item.image.url}` : "/images/no-image.png",
   };
+
+  return { post };
 }
 
 const BlogPostDetailsPage = ({ loaderData }: Route.ComponentProps) => {
-  const { postMeta, markdown } = loaderData;
+  const { post } = loaderData;
   return (
     <div className="max-w-3xl mx-auto px-6 py-12 bg-gray-900">
-      <h1 className="text-3xl font-bold text-blue-400 mb-2">
-        {postMeta.title}
-      </h1>
+      <h1 className="text-3xl font-bold text-blue-400 mb-2">{post.title}</h1>
       <p className="text-sm text-gray-400 mb-6">
-        {new Date(postMeta.date).toDateString()}
+        {new Date(post.date).toDateString()}
       </p>
+      <img
+        src={post.image}
+        alt={post.title}
+        className="w-full h-64 object-cover mb-4"
+      />
       <div className="prose prose-invert max-w-none mb-12">
-        <ReactMarkdown>{markdown}</ReactMarkdown>
+        <ReactMarkdown>{post.body}</ReactMarkdown>
         <Link
           to="/blog"
           className="inline-block bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition"
